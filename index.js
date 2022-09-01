42e34e2c7202c8fe172e3085b19c654b05bbd0d7
Surviavlist <59367337+Surviavlist@users.noreply.github.com>
2022-09-01 16:19:46 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const splitSkills = () => {
-  let skills = document.getElementById("skillsId");
-  skills.innerHTML = skills.innerHTML.split(",").join(" • ");
-}
-
-const onload = () => {
-  splitSkills();
-}
