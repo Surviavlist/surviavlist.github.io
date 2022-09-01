e031ddb3e540b8036ca52e85b0ee50648c77a361
Surviavlist <59367337+Surviavlist@users.noreply.github.com>
2022-09-01 12:29:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function splitSkills() {
+const splitSkills = () => {
   let skills = document.getElementById("skillsId");
   skills.innerHTML = skills.innerHTML.split(",").join(" • ");
+}
+
+const onload = () => {
+  splitSkills();
 }
