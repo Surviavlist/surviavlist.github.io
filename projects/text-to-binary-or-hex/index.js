0082ecc0665d25c004524fa58462b2af544f9e06
Surviavlist <59367337+Surviavlist@users.noreply.github.com>
2022-09-25 11:57:57 +0300

    Create index.js

---
+++ projects/text-to-binary-or-hex/index.js
@@ -0,0 +1,31 @@
+const input = document.getElementById('in')
+const output = document.getElementById('out')
+const to = document.getElementById('to')
+const delimiter = document.getElementById('delimiter')
+
+String.prototype.toHEX = function() {
+  let coded = []
+  for (let i = 0; i < this.length; i++) {
+    coded.push(this.charCodeAt(i).toString(16))
+  }
+  return coded.join(delimiter.value)
+}
+
+String.prototype.toBINARY = function() {
+  let coded = []
+  for (let i = 0; i < this.length; i++) {
+    coded.push(this.charCodeAt(i).toString(2))
+  }
+  return coded.join(delimiter.value)
+}
+
+function convert() {
+  let out = ''
+  let text = input.value
+  try {
+    out = ( to.value == 'binary' ? text.toBINARY() : text.toHEX() )
+  } catch(error) {
+    alert(error)
+  }
+  output.value = out
+}
